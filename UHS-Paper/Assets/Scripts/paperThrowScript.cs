c4beb4ee5ea44422a8e2913f2dc0eaf9f6bc2d0f
inroma <romain.foray@gmail.com>
2018-03-15 11:04:12 +0100

    correction scene

---
+++ UHS-Paper/Assets/Scripts/paperThrowScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperThrowScript : MonoBehaviour {
+
+    public float weight;
+    private float direction;
+    public float power;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
