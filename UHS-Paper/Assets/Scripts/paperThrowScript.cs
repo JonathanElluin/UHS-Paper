10a011664a5bf4e99d8065a68a04cba949e647de
inroma <romain.foray@gmail.com>
2018-03-28 16:58:31 +0200

    romain
    redéfinition de la scène, travail du script

--- UHS-Paper/Assets/Scripts/paperThrowScript.cs
+++ UHS-Paper/Assets/Scripts/paperThrowScript.cs
@@ -5,24 +5,41 @@ using UnityEngine;
 public class PaperThrowScript : MonoBehaviour {
 
     public GameObject paper;
+    public GameObject target;
     public float weight;
-    private Vector3 direction;
+    private int direction = 0;
     private Quaternion rotation;
-    public float power;
+    public float speed;
     public Move move;
 
     // Use this for initialization
     void Start () {
-        direction = move.wantedPosition;
+        //direction = move.wantedPosition;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKey("space"))
         {
-            paper.transform.SetPositionAndRotation(direction, rotation);
+            OnKeyPressed();
         }
+    }
 
+    void OnKeyPressed()
+    {
+        bool firstLaunch = true;
+        if (firstLaunch == true)
+        {
+            paper.GetComponent<Rigidbody>().useGravity = true;
+            paper.GetComponentInChildren<Rigidbody>().AddTorque(target.transform.position);
+            paper.GetComponent<Rigidbody>().AddForce(target.transform.position);
+        }
+        else
+        {
+
+        }
     }
 }
+
+
