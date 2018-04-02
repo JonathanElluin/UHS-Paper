44c24a8835dcfcb08b9be483db60771e50ff1fe6
inroma <romain.foray@gmail.com>
2018-04-02 15:43:15 +0200

    azerty
    avancement du script de la balle

--- UHS-Paper/Assets/Scripts/paperThrowScript.cs
+++ UHS-Paper/Assets/Scripts/paperThrowScript.cs
@@ -1,44 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PaperThrowScript : MonoBehaviour {
 
-    public GameObject paper;
-    public GameObject target;
-    public float weight;
-    private int direction = 0;
-    private Quaternion rotation;
-    public float speed;
-    public Move move;
+    public Rigidbody paper;
+    public Rigidbody target;
+    private Vector3 InitialPosition;
+
+    private Vector3 direction;
+
+    public float power;
+    private float xAxisForce;
+    private float yAxisForce;
+
+    public Text score;
+    private int paperScore = 0;
+
+    private bool canLaunch = true;
 
     // Use this for initialization
     void Start () {
         //direction = move.wantedPosition;
-
+        InitialPosition = this.transform.position;
+        paper.useGravity = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey("space"))
         {
-            OnKeyPressed();
+            OnKeydown();
         }
     }
 
-    void OnKeyPressed()
+    void OnKeydown()
     {
-        bool firstLaunch = true;
-        if (firstLaunch == true)
+        if (canLaunch == true)
         {
-            paper.GetComponent<Rigidbody>().useGravity = true;
-            paper.GetComponentInChildren<Rigidbody>().AddTorque(target.transform.position);
-            paper.GetComponent<Rigidbody>().AddForce(target.transform.position);
-        }
-        else
-        {
+            canLaunch = false;
+            xAxisForce = target.position.x - paper.position.x;
+            yAxisForce = target.position.y - paper.position.y;
 
+            direction = new Vector3(xAxisForce, yAxisForce*1.6f, -power);
+            paper.useGravity = true;
+            paper.velocity = direction;
+            
+            Destroy(gameObject, 5.0f);
+            ResetPosition();
         }
+    }
+    
+
+    private void ResetPosition()
+    {
+        this.transform.position = InitialPosition + new Vector3(Random.Range(-20, 20), 0f, 0f);
+        paper.useGravity = false;
+        canLaunch = true;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "trashtrigger")
+        {
+            ScoreUpdate();
+        }
+    }
+
+    private void ScoreUpdate()
+    {
+        paperScore++;
+        score.text = "Nombre de panier: " + paperScore.ToString();
     }
 }
 
